670b159ff033ced10757e9db183af00444fcde8d
Eric <etk3355@gmail.com>
2025-12-31 16:51:51 -0800

    Search Movies Component

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchMovies from '../searchMovies';
 
 class Main extends React.Component {
   render() {
     return (
       <div className='container'>
       <h1 className='title'>AllFilm</h1>
+      <SearchMovies />
       </div>
     );
   }
