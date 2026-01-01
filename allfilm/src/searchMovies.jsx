d9e2a46d45f90f18f9b0040b7fe400daa35ad477
Eric <etk3355@gmail.com>
2025-12-31 22:51:41 -0800

    Wrapping Children

--- allfilm/src/searchMovies.jsx
+++ allfilm/src/searchMovies.jsx
@@ -22,6 +22,7 @@ export default function SearchMovies(){
     }
     
     return (
+        <>
         <form className="form" onSubmit={searchMovies}>
             <label className="label" htmlFor="query">Movie Name</label>
             <input className="input" type="text" name="query"
@@ -30,5 +31,6 @@ export default function SearchMovies(){
                 />
             <button className="button" type="submit">Search</button>
         </form>
+        </>
     )
 }
