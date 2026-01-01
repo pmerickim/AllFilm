37bc4a0ba74454bb2096a5816bc52e5adcbf07c4
Eric <etk3355@gmail.com>
2025-12-31 23:14:49 -0800

    Rending Movie Cards Using MovieCard Function

--- allfilm/src/searchMovies.jsx
+++ allfilm/src/searchMovies.jsx
@@ -1,6 +1,5 @@
 import React, {useState} from "react";
-import MovieCard from './movieCard.jsx';
-export default function SearchMovies(){
+ export default function SearchMovies(){
     
     //states- input query, movies
     const [query, setQuery] = useState('');
@@ -9,7 +8,7 @@ export default function SearchMovies(){
     
     const searchMovies = async (e) => {
         e.preventDefault();
-        
+                
         const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
         
         try {
@@ -23,19 +22,19 @@ export default function SearchMovies(){
     
     return (
         <>
-        <form className="form" onSubmit={searchMovies}>
-            <label className="label" htmlFor="query">Movie Name</label>
-            <input className="input" type="text" name="query"
-                placeholder="i.e. Jurassic Park"
-                value={query} onChange={(e) => setQuery(e.target.value)}
-                />
-            <button className="button" type="submit">Search</button>
-        </form>
-        <div className="card-list">
+            <form className="form" onSubmit={searchMovies}>
+                <label className="label" htmlFor="query">Movie Name</label>
+                <input className="input" type="text" name="query"
+                    placeholder="i.e. Jurassic Park"
+                    value={query} onChange={(e) => setQuery(e.target.value)}
+                    />
+                <button className="button" type="submit">Search</button>
+            </form>
+            <div className="card-list">
                 {movies.filter(movie => movie.poster_path).map(movie => (
-                    </div>
-                    ))}
-                </div>   
-            </>
+                   <MovieCard />
+                ))}
+            </div>    
+        </>
     )
 }
