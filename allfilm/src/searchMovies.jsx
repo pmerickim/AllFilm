d67dc6815414af4a412d17aae38fa561ebfd4506
Eric <etk3355@gmail.com>
2025-12-31 22:48:21 -0800

    Setting the State

--- allfilm/src/searchMovies.jsx
+++ allfilm/src/searchMovies.jsx
@@ -1,26 +1,23 @@
-import React from "react";
 import React, {useState} from "react";
-
 
 export default function SearchMovies(){
     
-    const [query, setQuery] = useState('')
-
-    const [movies, setMovies] = useState([])
-
+    //states- input query, movies
+    const [query, setQuery] = useState('');
+    //create the state for movies, and update that state appropriate
+    const [movies, setMovies] = useState([]);
+    
     const searchMovies = async (e) => {
-        e.preventDefault()
-        
-        const query = "Jurassic Park";
+        e.preventDefault();
         
         const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
         
         try {
-            const res = await fetch(url)
-            const data  = await res.json()
-            setMovies(data.results)
+            const res = await fetch(url);
+            const data  = await res.json();
+            setMovies(data.results);
         }catch(err){
-            console.error(err)
+            console.error(err);
         }
     }
     
@@ -28,8 +25,9 @@ export default function SearchMovies(){
         <form className="form" onSubmit={searchMovies}>
             <label className="label" htmlFor="query">Movie Name</label>
             <input className="input" type="text" name="query"
-                placeholder="i.e. Jurassic Park"/>
-                value={query} onChange{(e) => setQuery(e.target.value)}
+                placeholder="i.e. Jurassic Park"
+                value={query} onChange={(e) => setQuery(e.target.value)}
+                />
             <button className="button" type="submit">Search</button>
         </form>
     )
