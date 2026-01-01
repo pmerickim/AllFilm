38be620f3a50195b98403463f49abef77160bf65
Eric <etk3355@gmail.com>
2025-12-31 23:00:30 -0800

    Cleaning Up the Syntax

--- allfilm/src/searchMovies.jsx
+++ allfilm/src/searchMovies.jsx
@@ -32,7 +32,7 @@ export default function SearchMovies(){
             <button className="button" type="submit">Search</button>
         </form>
         <div className="card-list">
-            {movies.filter(movie => map(movie => (
+                {movies.filter(movie => movie.poster_path).map(movie => (
                     <div className="card" key={movie.id}>
                         <img className="card--image"
                             src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
