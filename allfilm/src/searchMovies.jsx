cb0b4ea24bf75fc95860d4a9098661cdc959e762
Eric <etk3355@gmail.com>
2025-12-31 23:01:14 -0800

    Adding an Additional Div

--- allfilm/src/searchMovies.jsx
+++ allfilm/src/searchMovies.jsx
@@ -38,6 +38,8 @@ export default function SearchMovies(){
                             src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
                             alt={movie.title + ' poster'}
                             />
+                        <div className="card--content">
+                        </div>
 
                     </div>
                     ))}
