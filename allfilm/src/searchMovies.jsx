ea0f69888f86d2a1c05b84030c369c09d1a1c1bf
Eric <etk3355@gmail.com>
2025-12-31 22:45:36 -0800

    Cleaning Up the Syntax

--- allfilm/src/searchMovies.jsx
+++ allfilm/src/searchMovies.jsx
@@ -16,8 +16,8 @@ export default function SearchMovies(){
         const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
         
         try {
-            const res = await fetch(url);
-            const data  = await res.json();
+            const res = await fetch(url)
+            const data  = await res.json()
             console.log(data.results)
         }catch(err){
             console.error(err)
